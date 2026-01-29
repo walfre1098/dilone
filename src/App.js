30404dc604ec8d093a2830b039e2f4889649559e
walfre1098 <walfrebautt1098@gmail.com>
2026-01-29 17:51:24 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <h1>UTECO TU UNIVERSIDAD</h1>
+      <h1>Walfre Bautista</h1>
+      <hr />
+     </div>
+
+     <div>
+    <h1>ENERO-MARZO-2026</h1>
+    <p>Desarrollo Full stack</p>
+     </div>
+      <img src="./ok2.jpg" />
+    </div>
+    
+  );
+}
+
+export default App;
